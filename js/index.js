3a96cb2a9fb6984c907a1742f4128c7e95f672d7
Vasyl Dumych <v.doomich@gmail.com>
2017-10-06 11:21:51 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -9,3 +9,36 @@ $(document).ready(function() {
         }
     });
 });
+
+
+
+
+
+
+
+$(function(){
+    $('form input[type=submit]').click(function(e){
+        const form = $('form')[0];
+        if(!form.checkValidity()){
+             return;
+        }
+        e.preventDefault();
+         $.ajax({
+            url: 'https://formspree.io/v.doomich@gmail.com',
+            method: 'POST',
+            data: { 
+                personName: $('#name').val(),
+                personEmail: $('#email').val(),
+                message: $('#message').val()
+            },
+            dataType: 'json'
+        }).done(function(){
+            form.reset();
+            $('#message-box').html('Дякуємо, ми звяжемось з вами найближчим часом!');
+        }).fail(function() {
+            $('#message-box').html('Error');
+        });
+    });
+    
+    
+});
